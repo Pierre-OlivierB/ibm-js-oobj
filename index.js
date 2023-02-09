3065a8df865072a870ad1785c6478506a4be62e6
unknown <baudinopierreolivier@gmail.com>
2023-02-09 13:38:01 +0100

    add car object

--- index.js
+++ index.js
@@ -34,10 +34,39 @@ class Chien {
   }
 }
 var waf = new Chien("Rex", "12", "Berger", "waf");
-waf.eat(15);
-waf.dogScream();
-waf.description();
-waf.eat(5);
-waf.dogScream();
-waf.qttEat();
-waf.description();
+// waf.eat(15);
+// waf.dogScream();
+// waf.description();
+// waf.eat(5);
+// waf.dogScream();
+// waf.qttEat();
+// waf.description();
+class Car {
+  constructor(mark, type, year, power) {
+    this.mark = mark;
+    this.type = type;
+    this.year = year;
+    this.power = power;
+    this.vitesse = 0;
+  }
+  showCar() {
+    console.log(
+      "Marque de la voiture : " +
+        this.mark +
+        ". Modèle : " +
+        this.type +
+        ". Année de fabrication : " +
+        this.year +
+        ". Cylindre : " +
+        this.power +
+        (this.vitesse === 0
+          ? ". La voiture est à l'arrêt"
+          : ". La voiture avance de " + this.vitesse + "km/h")
+    );
+  }
+}
+var firstCar = new Car("Renault", "R5", 1990, 2);
+var secondCar = new Car("Citroën", "C4", 2000, 4);
+var thirdCar = new Car("Tesla", "L", 2015, 10);
+
+firstCar.showCar();
